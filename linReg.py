11d8180b0144da6d139076490c09542264b3443f
irinaradanovic <irina.radanovic5@gmail.com>
2025-08-31 14:47:29 +0200

    pocetak

---
+++ linReg.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import ast
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import seaborn as sn
+import numpy as np
+from utils_nans1 import *
